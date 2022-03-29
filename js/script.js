cca76312412a307b11cf124fb72a461f96839886
Alfo07 <ammiratialfonso07@gmail.com>
2022-03-29 17:31:02 +0200

    Struttura

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//MAIL
+//Creare un imput in cui l'utente inserisce la sua mail
+//Creare un array con dentro 10 mail
+//Creare un for e un if per il controllo della mail inserita dall'utente
+
+//DADI
+//Creare due array in cui sono contenuti 6 numeri, uno per il giocatore e uno per il computer
+//Prendere due numeri casuali dall'array e creare un if che li mette a confronto
+//Il vincitore è quello con il numero più alto
